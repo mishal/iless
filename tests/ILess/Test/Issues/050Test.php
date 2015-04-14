3b7575d2c3a3f001b8d2b7d8be03feeb277aa1b7
Michal Moravec <michi.m@gmail.com>
2015-04-14 09:28:30 +0200

    Added test for issue #50

---
+++ tests/ILess/Test/Issues/050Test.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the ILess
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Issue #50 test
+ *
+ * @package ILess
+ * @subpackage test
+ */
+class ILess_Test_Issues_050Test extends ILess_Test_TestCase
+{
+    public function testIssue()
+    {
+        $parser = new ILess_Parser(array(
+            'compress' => false
+        ));
+
+        $parser->parseString('@swatch: foobar;
+@import "../../../bootstrap3/less/@{swatch}/variables.less";
+');
+
+        $this->setExpectedException('ILess_Exception_Import', '/bootstrap3/less/foobar/variables.less');
+
+        $css = $parser->getCSS();
+    }
+
+}
